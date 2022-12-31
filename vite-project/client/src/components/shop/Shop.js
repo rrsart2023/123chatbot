2e359689230c2c45bca662a35c15c814cd2e6594
rrsartpl <rrsartpl@gmail.com>
2022-12-31 17:50:06 +0100

    first commit

---
+++ vite-project/client/src/components/shop/Shop.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ItemList from './ItemList';
+
+const Shop = () => (
+    <div>
+        <h1>Shop</h1>
+        <ItemList />
+    </div>
+)
+
+export default Shop;
+
